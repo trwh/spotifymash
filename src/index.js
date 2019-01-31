9ecd93277011b29e8e00051d11db740c42110f9e
Tim Harrison <mail@trwh.co.uk>
2019-01-31 16:00:26 +0000

    Get Spotify Client authentication working.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const SpotifyWebApi = require('spotify-web-api-node');
+require('dotenv').config();
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
 
 const app = express();
 
@@ -9,3 +16,32 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 })
+
+authenticateSpotify();
+
+function authenticateSpotify() {
+  spotifyApi.clientCredentialsGrant().then(
+    function(data) {
+      spotifyApi.setAccessToken(data.body['access_token']);
+      console.log('The access token is ' + spotifyApi.getAccessToken());
+      // onceAuthenticatedDo();
+    },
+    function(err) {
+      console.log('Something went wrong when retrieving an access token', err);
+    }
+  );
+}
+
+// function onceAuthenticatedDo() {
+
+//   spotifyApi.getArtistAlbums('06HL4z0CvFAxyc27GXpf02').then(
+//     function(data) {
+//       console.log('Artist albums', data.body);
+//     },
+//     function(err) {
+//       console.error(err);
+//     }
+//   );
+
+// }
+
